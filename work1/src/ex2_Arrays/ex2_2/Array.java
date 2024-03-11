9dc11a4c38f57183699069c6f24554a4902c49f3
@aleksei_neverko <aaneverko@mail.ru>
2024-03-12 02:30:37 +0300

    exercise 2.2

--- work1/src/ex2_Arrays/ex2_2/Array.java
+++ work1/src/ex2_Arrays/ex2_2/Array.java
@@ -1,2 +1,54 @@
-package ex2_Arrays.ex2_2;public class Array {
+package ex2_Arrays.ex2_2;
+
+public class Array {
+    private final int[] arr;
+
+    public Array(int[] arr) {
+        this.arr = arr;
+    }
+    public void DisplayFor() {
+        for (int i = 0; i <= arr.length-1; i++) {
+            int num = arr[i];
+            int remainder = i % 2;
+            if (i != arr.length-1) {
+                if (remainder == 1) {
+                    System.out.printf("%d;", num);
+                }
+            } else {
+                System.out.printf("%d >>> \n", num);
+            }
+
+        }
+    }
+    public void DisplayDoWhile() {
+        int i = 0;
+        do {
+            int length = arr[i];
+            int remainder = i % 2;
+            if (i == arr.length-1) {
+                System.out.printf("%d >> \n", length);
+
+            } else {
+                if (remainder == 1) {
+                    System.out.printf("%d!", length);
+                }
+            }
+            i ++;
+        } while (i <= arr.length-1);
+    }
+    public void DisplayWhile() {
+        int i = 0;
+        while (i <= arr.length-1) {
+            int remainder = i % 2;
+            int length = arr[i];
+            if (i == arr.length-1) {
+                System.out.printf("%d => \n", length);
+            } else {
+                if (remainder == 1) {
+                    System.out.printf("%d:", length);
+                }
+            }
+            i ++;
+        }
+    }
 }
--- work1/src/ex2_Arrays/ex2_2/Program.java
+++ work1/src/ex2_Arrays/ex2_2/Program.java
@@ -1,2 +1,10 @@
-package ex2_Arrays.ex2_2;public class Program {
+package ex2_Arrays.ex2_2;
+
+public class Program {
+    public static void main(String[] args) {
+        Array array = new Array(new int[]{1, 2, 7, 12, 5, 15, 7, 99, 9, 102});
+        array.DisplayFor();
+        array.DisplayDoWhile();
+        array.DisplayWhile();
+    }
 }
