651eb464a4af16cff6a51d5720c5353919db558d
@aleksei_neverko <aaneverko@mail.ru>
2024-03-16 01:57:27 +0300

    fix exercise 2.3 method name camelCase

--- work1/src/ex2_Arrays/ex2_2/Program.java
+++ work1/src/ex2_Arrays/ex2_2/Program.java
@@ -4,8 +4,8 @@ public class Program {
     public static void main(String[] args) {
         Array array = new Array(new int[]{1, 2, 7, 12, 5, 15, 7, 99, 9, 102, 21});
         Array array2 = new Array(new int[]{1, 2, 7, 12, 5, 15, 7, 99, 9, 102, 21, 22, 18});
-        array.DisplayFor();
-        array2.DisplayDoWhile();
-        array.DisplayWhile();
+        array.displayFor();
+        array2.displayDoWhile();
+        array.displayWhile();
     }
 }
